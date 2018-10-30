b4ee6d221f684e9f25d1463f445686608a65038c
BrianD147 <briandoyle147@gmail.com>
2018-10-30 15:37:33 +0000

    Added player movement for testing

---
+++ 4th-Year-Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb; // Players rigidbody
+    public float moveSpeed; // Players movement speed
+
+	// Use this for initialization
+	void Start () {
+        rb.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+    }
+}
