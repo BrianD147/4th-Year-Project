dbe0899c6ad93e04b06ef5f3533ff1511482c539
BrianD147 <briandoyle147@gmail.com>
2018-10-30 21:21:29 +0000

    Added kickForce to the ball on collision

--- 4th-Year-Project/Assets/Scripts/PlayerController.cs
+++ 4th-Year-Project/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
     public Rigidbody rb; // Players rigidbody
     public float moveSpeed; // Players movement speed
+    public float kickPower; // Used to apply a kick to the ball
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +17,17 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
         transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
         transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        float force = 2000f * kickPower;
+        if (c.gameObject.tag == "ball")
+        {
+            Vector3 dir = c.contacts[0].point - transform.position;
+            dir = dir.normalized;
+            c.gameObject.GetComponent<Rigidbody>().AddForce(dir * force);
+        }
+
     }
 }
