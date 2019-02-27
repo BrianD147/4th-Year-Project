bc484a2074b5badc7854be65bf89689c26948ddf
RCElucidator7 <G00332826@gmit.ie>
2019-02-27 12:41:33 +0000

    Ball reset position when entering goal

---
+++ 4th-Year-Project/Assets/Scripts/ResetBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision c)
+    {
+		Debug.Log(c);
+        if (c.gameObject.tag == "redGoal")
+        {
+			transform.position = new Vector3(0f, 1.5f, 0f);
+        }
+
+    }
+}
