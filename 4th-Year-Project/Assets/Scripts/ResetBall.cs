5082e18ba6de6c7ae3d679b7e73d70f177b6f2ec
BrianD147 <briandoyle147@gmail.com>
2019-04-25 22:55:03 +0100

    Added player position reset

--- 4th-Year-Project/Assets/Scripts/ResetBall.cs
+++ 4th-Year-Project/Assets/Scripts/ResetBall.cs
@@ -8,10 +8,24 @@ public class ResetBall : MonoBehaviour {
 	private GameObject canvas;
 	private ScoreTrack sc;
 
+	private GameObject blueGoalie;
+	private GameObject redGoalie;
+	private GameObject bluePlayer;
+	private GameObject redPlayer;
+	private GameObject blueDefender;
+	private GameObject redDefender;
+
 	// Use this for initialization
 	void Start () {
 		canvas = GameObject.FindWithTag("canvas");
 		sc = canvas.GetComponent<ScoreTrack>();
+
+		blueGoalie = GameObject.FindWithTag("blueGoalie");
+		redGoalie = GameObject.FindWithTag("redGoalie");
+		bluePlayer = GameObject.FindWithTag("bluePlayer");
+		redPlayer = GameObject.FindWithTag("redPlayer");
+		blueDefender = GameObject.FindWithTag("blueDefender");
+		redDefender = GameObject.FindWithTag("redDefender");
 	}
 	
 	// Update is called once per frame
@@ -27,6 +41,7 @@ public class ResetBall : MonoBehaviour {
 			transform.position = new Vector3(0f, 1.5f, 0f);
 			sc.BlueScore();
 			Debug.Log("Blue Goal!");
+			ResetPlayers();
         }
 
 		if (c.gameObject.tag == "blueGoal")
@@ -36,4 +51,29 @@ public class ResetBall : MonoBehaviour {
 			Debug.Log("Red Goal!");
         }
     }
+
+	private Random random = new Random();
+
+	float RandomNumberBetween(float minValue, float maxValue)
+	{
+	    float random = Random.Range(minValue, maxValue);
+
+	    return random;
+	}
+
+	void ResetPlayers(){
+		float Offset;
+		blueGoalie.transform.position = new Vector3(-13, 0, 0);
+		redGoalie.transform.position = new Vector3(13, 0, 0);
+
+		Offset = RandomNumberBetween(-5, -1);
+		bluePlayer.transform.position = new Vector3(Offset, 0, -1);
+		Offset = RandomNumberBetween(1, 5);
+		redPlayer.transform.position = new Vector3(Offset, 0, 1);
+
+		Offset = RandomNumberBetween(-8, -4);
+		blueDefender.transform.position = new Vector3(Offset, 0, 1);
+		Offset = RandomNumberBetween(4, 8);
+		redDefender.transform.position = new Vector3(Offset, 0, -1);
+	}
 }
